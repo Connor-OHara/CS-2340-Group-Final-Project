31bf901f69f462566120748c313301cfba01fe74
Connor-OHara <52166249+Connor-OHara@users.noreply.github.com>
2021-06-21 16:07:25 -0400

    Added Checking Software
    Navigate to project directory and run python3 run_checkstyle.py if you have python installed and Java root set, it will give a rating for code cleanliness. More info on Canvas.

--- JavaFXGame/src/main/Main.java
+++ JavaFXGame/src/main/Main.java
@@ -1,8 +1,8 @@
 package main;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -16,22 +16,22 @@ public class Main extends Application {
 
     //sets up main program
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
 
         Pane root = new Pane();
-        Scene titleScene = new Scene(root, screenWidth,screenHeight);
+        Scene titleScene = new Scene(root, screenWidth, screenHeight);
 
-        Button newButt= new Button("New Game");
+        Button newButt = new Button("New Game");
         Button contButt = new Button("Continue");
         Button optButt = new Button("Options");
         Button leadButt = new Button("Leaderboard");
         Button exitButt = new Button("Exit");
 
         GridPane startGrid = new GridPane();
-        startGrid.add(newButt, 0,0);
-        startGrid.add(contButt, 0,1);
-        startGrid.add(optButt, 1,0);
-        startGrid.add(leadButt, 1,1);
+        startGrid.add(newButt, 0, 0);
+        startGrid.add(contButt, 0, 1);
+        startGrid.add(optButt, 1, 0);
+        startGrid.add(leadButt, 1, 1);
         //startGrid.add(newButt, 0,0);
         startGrid.setHgap(30);
         startGrid.setVgap(30);
