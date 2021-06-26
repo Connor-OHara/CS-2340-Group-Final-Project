f5508f0dd842f78cec77932c57869ebcd2601da0
Matthew JS <mstadter3@gatech.edu>
2021-06-26 17:03:09 -0400

    Adding tests to check that player config translates to game values

--- src/test/java/com/group19/javafxgame/InitGameTest.java
+++ src/test/java/com/group19/javafxgame/InitGameTest.java
@@ -19,19 +19,20 @@ public class InitGameTest {
     public void testSprite() {
         /**
          * To run this test, start the game and select the weapon type associated with the image
-         * in a texture is created from in like 29.
+         * in a texture is created from in like 27.
          * Sword: swordsman.png
          * Shuriken: ninja.png
          * Shield: shieldsman.png
          */
         Texture texture = FXGL.texture("ninja.png");
-        assertEquals(texture.getImage(), CharacterFactory.texture.getImage());
+        assertEquals(texture.getImage(), CharacterFactory.getTexture().getImage());
     }
 
     @Test
     public void testMoney() {
         /**
-         * To run this test, start the game and select the difficulty associated with the money in line 41.
+         * To run this test, start the game and select the difficulty
+         * associated with the money in line 40.
          * Beginner: 10
          * Intermediate: 5
          * Veteran: 1
